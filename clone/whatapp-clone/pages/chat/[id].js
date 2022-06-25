e84e8754bea1f44245c569792452f9f0018e9667
begginer pp <pp@pmsplus.co.kr>
2022-06-25 09:47:48 +0900

    폴더 정리

--- clone/whatapp-clone/pages/chat/[id].js
+++ clone/whatapp-clone/pages/chat/[id].js
@@ -1,55 +1,60 @@
-import styled from "styled-components"; 
-import Head from 'next/head';
-import Sidebar from '../../components/Sidebar';
-import ChatScreen from '../../components/ChatScreen';
-import { auth, db } from '../../firebase';
-import { useAuthState } from 'react-firebase-hooks/auth'; 
-import getRecipientEmail from '../../utils/getRecipientEmail';
+import styled from "styled-components";
+import Head from "next/head";
+import Sidebar from "../../components/Sidebar";
+import ChatScreen from "../../components/ChatScreen";
+import { auth, db } from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getRecipientEmail from "../../utils/getRecipientEmail";
 
-function Chat({chat, messages}) {
+function Chat({ chat, messages }) {
   const [user] = useAuthState(auth);
 
   return (
-    <Container >
+    <Container>
       <Head>
         <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
       <Sidebar />
       <ChatContainer>
-        <ChatScreen chat={chat} messages={messages}/>
+        <ChatScreen chat={chat} messages={messages} />
       </ChatContainer>
-    </Container >
-  )
+    </Container>
+  );
 }
 
 export default Chat;
 
 export async function getServerSideProps(context) {
-  const ref = db.collection('chats').doc(context.query.id);
+  const ref = db.collection("chats").doc(context.query.id);
 
   // Prepare the messages on the server
-  const messageRes = await ref.collection('messages').orderBy('timestamp', 'asc').get();
-  const messages = messageRes.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data()
-  })).map(messages => ({
-    ...messages,
-    timestamp: messages.timestamp.toDate().getTime()
-  }))
+  const messageRes = await ref
+    .collection("messages")
+    .orderBy("timestamp", "asc")
+    .get();
+  const messages = messageRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
 
   // Prepare the chats
   const chatRes = await ref.get();
-  const chat = { 
+  const chat = {
     id: chatRes.id,
-    ...chatRes.data()
-  }
+    ...chatRes.data(),
+  };
 
   return {
     props: {
       messages: JSON.stringify(messages),
-      chat: chat
-    }
-  }
+      chat: chat,
+    },
+  };
 }
 
 const Container = styled.div`
